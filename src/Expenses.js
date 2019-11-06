12ac29ea9706ca1479b6487143827e1e4762b4ca
tjortiz17 <tjortiz17@gmail.com>
2019-11-06 10:05:22 -0800

    updated UI and minor code cleanup

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,5 +1,104 @@
-import React, {Component} from "react";
+import React from "react";
 import fire from "./config/firebase";
+//import "./Expenses.css";
+//import Chevron from "./Chevron";
+//expansion panel files
+import {makeStyles} from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+//expense table files
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+function Accordion(){
+	const useStyles = makeStyles(theme => ({
+		root: {
+			width: '100%',
+		},
+		heading: {
+			fontSize: theme.typography.pxToRem(15),
+			fontWeight: theme.typography.fontWeightRegular,
+		},
+	}));
+
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<ExpansionPanel>
+				<ExpansionPanelSummary
+					expandIcon={<ExpandMoreIcon />}
+					aria-controls="panel1a-content"
+					id="panel1a-header"
+				>
+					<Typography className={classes.heading}>
+						This Months's Expenses
+					</Typography>
+				</ExpansionPanelSummary>
+				<ExpansionPanelDetails>
+					<Typography>
+						<ExpenseTable />
+					</Typography>
+				</ExpansionPanelDetails>
+			</ExpansionPanel>
+		</div>
+	);
+}
+
+function ExpenseTable(){
+	const useStyles = makeStyles({
+		root: {
+			width: '100%',
+			overflowX: 'auto',
+		},
+		table: {
+			minWidth: 650,
+		},
+	});
+	function createExpense(name, amount, category, date){
+		return {name, amount, category, date};
+	}
+	var expenses = [
+		createExpense('Whole Foods', 45, 'Groceries', '11/6/2019'),
+		createExpense('Movie', 30, 'Entertainment', '10/31/2019'),
+	];
+
+	const classes = useStyles();
+
+	return (
+		<Paper className={classes.root}>
+			<Table className={classes.table} aria-label="Expenses">
+				<TableHead>
+					<TableRow>
+						<TableCell>Expense</TableCell>
+						<TableCell align="right">Amount</TableCell>
+						<TableCell align="right">Category</TableCell>
+						<TableCell align="right">Date</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{expenses.map(expenses=> (
+						<TableRow key = {expenses.index}>
+							<TableCell component="th" scope="row">
+								{expenses.name}
+							</TableCell>
+							<TableCell align="right">{expenses.amount}</TableCell>
+							<TableCell align="right">{expenses.category}</TableCell>
+							<TableCell align="right">{expenses.date}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</Paper>
+	);
+}
 
 class ExpenseForm extends React.Component{
 	constructor(props){
@@ -192,12 +291,7 @@ class Expenses extends React.Component {
 	render(){
 		return (
 			<div>
-				<h1>This Month's Expenses</h1>
-				
-				<div>
-				<ExpenseForm />
-				<button>Add Expense</button>
-				</div>
+				<Accordion />
 			</div>
 		);
 	}
