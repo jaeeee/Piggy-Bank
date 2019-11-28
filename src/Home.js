fd5f2c2d1980cf5162ffcc074f8f654ae09c498e
Daniel Kwong <dkwon014@ucr.edu>
2019-11-28 00:04:00 -0800

    remove redundant database call

--- src/Home.js
+++ src/Home.js
@@ -20,39 +20,9 @@ class Home extends Component {
       testing: "",
     };
     this.logout = this.logout.bind(this);
-    this.grabName = this.grabName.bind(this);
+    // this.grabName = this.grabName.bind(this);
   }
-
-  grabName() {
-    var final = "";
-    // console.log("Before");
-    // console.log(final);
-    var docRef = db.collection("users").doc(fire.auth().currentUser.email);
-    docRef.get().then(function(documentSnapshot) {
-      // console.log(documentSnapshot.data());
-        // var converted = JSON.stringify(documentSnapshot.data())
-        // console.log(converted);
-        // console.log(documentSnapshot.data().name);
-        // alert(converted.name);
-        // console.log(converted.name);
-        // return converted[name];
-        // console.log();
-        // this.state.testing = documentSnapshot.data().name;
-        // final = documentSnapshot.data().name;
-        final = documentSnapshot.data().name;
-        // this.setState.testing = "yes";
-        // alert("Welcome back, " + final);
-        // this.setState.testing = final;
-        return final;
-    });
-    // console.log("After");
-    // console.log(final);
-    // return final;
-    // var name = "YES
-    // "
-    // return name;
-  }
-
+  
   logout() {
     fire.auth().signOut();
   }
@@ -66,6 +36,7 @@ class Home extends Component {
 
       // {/* No header, and the drawer stays open on larger screens (fixed drawer). */}
       <div>
+        
         {/* <Navbar /> */}
         <MDBContainer>
           <MDBCard>
