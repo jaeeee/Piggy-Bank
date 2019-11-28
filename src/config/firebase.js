c0e708826b6dc8d6091580de5dac9bc86d67efdd
Daniel Kwong <dkwon014@ucr.edu>
2019-11-28 00:20:31 -0800

    Set to correct db

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -15,8 +15,8 @@ import firebase from 'firebase';
   //   appId: "1:677504131374:web:7d80a2b5b5a3b094ed5abf"
   // };
 
-  /*
-   * BACKUP PIGGY DATABASE
+  
+   // BACKUP PIGGY DATABASE
 
    var firebaseConfig = {
      apiKey: "AIzaSyBNGQ94b3sh_CA0Xud_2RKTDZcrmCSV1aQ",
@@ -27,19 +27,19 @@ import firebase from 'firebase';
      messagingSenderId: "314565949487",
      appId: "1:314565949487:web:e5b4472d9b1da8b367aeda",
      measurementId: "G-VDJS8GGM8B"
-   };*/
+   };
 
   //  //BACKUP PIGGY DATABASE 2
-   var firebaseConfig = {
-    apiKey: "AIzaSyB-3c-A7D9wL760suIlG1rpuaoHyCMnkcA",
-    authDomain: "piggybankbackup2.firebaseapp.com",
-    databaseURL: "https://piggybankbackup2.firebaseio.com",
-    projectId: "piggybankbackup2",
-    storageBucket: "piggybankbackup2.appspot.com",
-    messagingSenderId: "735217764135",
-    appId: "1:735217764135:web:70c868ce0316074a32cb02",
-    measurementId: "G-HZCM51BE1D"
-  };
+  //  var firebaseConfig = {
+  //   apiKey: "AIzaSyB-3c-A7D9wL760suIlG1rpuaoHyCMnkcA",
+  //   authDomain: "piggybankbackup2.firebaseapp.com",
+  //   databaseURL: "https://piggybankbackup2.firebaseio.com",
+  //   projectId: "piggybankbackup2",
+  //   storageBucket: "piggybankbackup2.appspot.com",
+  //   messagingSenderId: "735217764135",
+  //   appId: "1:735217764135:web:70c868ce0316074a32cb02",
+  //   measurementId: "G-HZCM51BE1D"
+  // };
 
   //BACKUP PIGGY 3
   // var firebaseConfig = {
