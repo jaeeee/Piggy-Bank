f3b43a27567b2fa6b5341e0a79b1b8a0df1b97d1
tjortiz17 <tjortiz17@gmail.com>
2019-11-29 11:51:17 -0800

    split classes for expense input and display

---
+++ src/ExpenseForm.js
@@ -0,0 +1,188 @@
+import React from "react";
+import { db } from "./config/firebase";
+import fire from "./config/firebase";
+import firebase from 'firebase';
+import {makeStyles} from '@material-ui/core/styles';
+//Form Dialog files
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+//actual form files
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
+import { array } from "prop-types";
+
+class ExpenseForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            amount: '',
+            category: '',
+            date: '',
+        }
+        //this.state = this.state.bind(this);
+        this.NewExpenseForm = this.NewExpenseForm.bind(this);
+        this.ExpenseFormDialog = this.ExpenseFormDialog.bind(this);
+        this.createExpense = this.createExpense.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    createExpense(name, amount, category, date){
+		return {name, amount, category, date};
+	}
+    
+    handleChange = prop => event => {
+        //setValues({ ...values, [prop]: event.target.value })
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+        console.log(this.state.name);
+        console.log(this.state.amount);
+        console.log(this.state.category);
+        console.log(this.state.date);
+    }
+
+    NewExpenseForm(){
+        const useStyles = makeStyles(theme => ({
+            root: {
+                display: 'flex',
+                flexWrap: 'wrap',
+            },
+            margin: {
+                margin: theme.spacing(1),
+            },
+            withoutLabel: {
+                marginTop: theme.spacing(3),
+            },
+            textField: {
+                width: 200,
+            },
+            menu: {
+                width: 200,
+            },
+        }));
+        const classes = useStyles();
+        const [values, setValues] = React.useState({
+            name: '',
+            amount: '',
+            category: '',
+            date: '',
+        });
+        const handleChange = prop => event => {
+            //setValues({ ...values, [prop]: event.target.value })
+            this.setState({
+                [prop]: event.target.value,
+            });
+            console.log(this.state.name);
+            console.log(this.state.amount);
+            console.log(this.state.category);
+            console.log(this.state.date);
+        }
+        return(
+            <div>
+                <FormControl className={classes.margin, classes.textField}>
+                    <InputLabel htmlFor="standard-adornment-name">Name</InputLabel>
+                    <Input
+                        id="standard-adornment-name"
+                        value={this.state.name}
+                        onChange={this.handleChange('name')}
+                    />
+                </FormControl>
+                <FormControl className={classes.margin, classes.textField}>
+                    <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+                    <Input
+                        id="standard-adornment-amount"
+                        value={this.state.amount}
+                        onChange={this.handleChange('amount')}
+                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                    />
+                </FormControl>
+                <FormControl className={classes.margin, classes.textField}>
+                    <InputLabel htmlFor="standard-adornment-category">Category</InputLabel>
+                    <Input
+                        id="standard-adornment-category"
+                        value={this.state.category}
+                        onChange={this.handleChange('category')}
+                    />
+                </FormControl>
+                <TextField
+                    id="date"
+                    label="Date of Expense"
+                    type="date"
+                    placeholder="YYYY-MM-DD"
+                    className={classes.textField}
+                    value={this.state.date}
+                    onChange={this.handleChange('date')}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </div>
+        );
+    }
+    ExpenseFormDialog(){
+        const useStyles = makeStyles(theme => ({
+            fab: {
+                margin: theme.spacing(1)
+            }
+        }));
+        const[open, setOpen] = React.useState(false);
+        const handleClickOpen = () => {
+            setOpen(true);
+        };
+        const handleClose = () => {
+            setOpen(false);
+        };
+        const handleAdd = () => {
+            //TODO add values to database
+            var purchase = [];
+            purchase = this.createExpense(this.state.name, this.state.amount, this.state.category, this.state.date);
+            let userRef = db.collection("users").doc(fire.auth().currentUser.email);
+            userRef.update({
+                expenses: firebase.firestore.FieldValue.arrayUnion(purchase)
+            });
+            setOpen(false);
+        }
+        const classes = useStyles();
+        return (
+            <div>
+                <Fab color="primary" aria-label="add" className={classes.fab} onClick={handleClickOpen}>
+                    <AddIcon />
+                </Fab>
+                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">New Expense</DialogTitle>
+                    <DialogContent>
+                        <this.NewExpenseForm />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={handleAdd} color="primary">
+                            Add
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        )
+    }
+    render(){
+        return(
+                <div>
+                    <this.ExpenseFormDialog />
+                </div>
+        );
+    }    
+    
+}
+export default ExpenseForm;
--- src/Expenses.js
+++ src/Expenses.js
@@ -1,5 +1,6 @@
 import React from "react";
 import fire from "./config/firebase";
+import ExpenseForm from "./ExpenseForm"
 // import Calculator from './layout/Calculator/Calculator';
 //expansion panel files
 import {makeStyles} from '@material-ui/core/styles';
@@ -15,240 +16,114 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-//Form Dialog files
-import Button from '@material-ui/core/Button';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-//actual form files
-import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
 
-var expenses;
-//var numExpenses = expenses.length;
-function createExpense(name, amount, category, date){
-	return {name, amount, category, date};
-}
-//expenses = [createExpense('Example', 1000, 'fun', '1/1/0001')];
-function addExpense(name, amount, category, date){
-	if(expenses[0].name === 'Example'){
-		expenses = [createExpense(name, amount, category, date)];
-	}else{
-		expenses.push(createExpense(name, amount, category, date));
-	}
-}
-
-function Accordion(){
-	const useStyles = makeStyles(theme => ({
-		root: {
-			width: '100%',
-		},
-		heading: {
-			fontSize: theme.typography.pxToRem(15),
-			fontWeight: theme.typography.fontWeightRegular,
-		},
-	}));
-
-	const classes = useStyles();
-
-	return (
-		<div className={classes.root}>
-			<ExpansionPanel>
-				<ExpansionPanelSummary
-					expandIcon={<ExpandMoreIcon />}
-					aria-controls="panel1a-content"
-					id="panel1a-header"
-				>
-					<Typography className={classes.heading}>
-						This Month's Expenses
-					</Typography>
-				</ExpansionPanelSummary>
-				<ExpansionPanelDetails>
-					<Typography>
-						<ExpenseTable />
-					</Typography>
-				</ExpansionPanelDetails>
-			</ExpansionPanel>
-		</div>
-	);
-}
-function ExpenseTable(){
-	const useStyles = makeStyles({
-		root: {
-			width: '100%',
-			overflowX: 'auto',
-		},
-		table: {
-			minWidth: 650,
-		},
-	});
-	
-	expenses = [createExpense('Whole Foods', 45, 'Groceries', '11/6/2019')];
-	expenses.push(createExpense('Movie', 30, 'Entertainment', '10/31/2019'));
-	expenses.push(createExpense('Disneyland', 300, 'Leisure', '11/10/2019'));
-	// addExpense('Whole Foods', 45, 'Groceries', '11/6/2019');
-	// addExpense('Movie', 30, 'Entertainment', '10/31/2019');
-	// addExpense('Disneyland', 300, 'Leisure', '11/10/2019');
-	const classes = useStyles();
-	return (
-		<Paper className={classes.root}>
-			<Table className={classes.table} aria-label="Expenses">
-				<TableHead>
-					<TableRow>
-						<TableCell>Expense</TableCell>
-						<TableCell align="right">Amount</TableCell>
-						<TableCell align="right">Category</TableCell>
-						<TableCell align="right">Date</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{expenses.map(expenses => (
-						<TableRow key = {expenses.index}>
-							<TableCell component="th" scope="row">
-								{expenses.name}
-							</TableCell>
-							<TableCell align="right">{expenses.amount}</TableCell>
-							<TableCell align="right">{expenses.category}</TableCell>
-							<TableCell align="right">{expenses.date}</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</Paper>
-	);
-}
-function ExpenseFormDialog(){
-	const useStyles = makeStyles(theme => ({
-		fab: {
-			margin: theme.spacing(1)
-		}
-	}));
-	const[open, setOpen] = React.useState(false);
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
-	const handleClose = () => {
-		setOpen(false);
-	};
-	const classes = useStyles();
-	return (
-		<div>
-			<Fab color="primary" aria-label="add" className={classes.fab} onClick={handleClickOpen}>
-				<AddIcon />
-			</Fab>
-			<Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-				<DialogTitle id="form-dialog-title">New Expense</DialogTitle>
-				<DialogContent>
-					<ExpenseForm />
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose} color="primary">
-						Cancel
-					</Button>
-					<Button onClick={handleClose} color="primary">
-						Add
-					</Button>
-				</DialogActions>
-			</Dialog>
-		</div>
-	)
-}
-function ExpenseForm(){
-	const useStyles = makeStyles(theme => ({
-		root: {
-			display: 'flex',
-			flexWrap: 'wrap',
-		},
-		margin: {
-			margin: theme.spacing(1),
-		},
-		withoutLabel: {
-			marginTop: theme.spacing(3),
-		},
-		textField: {
-			width: 200,
-		},
-		menu: {
-			width: 200,
-		},
-	}));
-	//category choices
-	/*const categories = [
-		{value: }
-	];*/
-	const classes = useStyles();
-	const [values, setValues] = React.useState({
-		name: '',
-		amount: '',
-		category: '',
-		date: '',
-	});
-	const handleChange = prop => event => {
-		setValues({ ...values, [prop]: event.target.value })
-	}
-	return(
-		<div>
-			<FormControl className={classes.margin, classes.textField}>
-				<InputLabel htmlFor="standard-adornment-name">Name</InputLabel>
-				<Input
-					id="standard-adornment-name"
-					value={values.name}
-					onChange={handleChange('name')}
-				/>
-			</FormControl>
-			<FormControl className={classes.margin, classes.textField}>
-				<InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
-				<Input
-					id="standard-adornment-amount"
-					value={values.amount}
-					onChange={handleChange('amount')}
-					startAdornment={<InputAdornment position="start">$</InputAdornment>}
-				/>
-			</FormControl>
-			<FormControl className={classes.margin, classes.textField}>
-				<InputLabel htmlFor="standard-adornment-category">Category</InputLabel>
-				<Input
-					id="standard-adornment-category"
-					value={values.category}
-					onChange={handleChange('category')}
-				/>
-			</FormControl>
-			<TextField
-				id="date"
-				label="Date of Expense"
-				type="date"
-				placeholder="YYYY-MM-DD"
-				className={classes.textField}
-				InputLabelProps={{
-					shrink: true,
-				}}
-			/>
-		</div>
-	);
-	
-}
 class Expenses extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			DATA: []
+			expenses: []
 		}
-		console.log(this.state.DATA);
+		this.ExpenseTable = this.ExpenseTable.bind(this);
+		this.Accordion = this.Accordion.bind(this);
 	}
+
+	createExpense(name, amount, category, date){
+		return {name, amount, category, date};
+	}
+	addExpense(name, amount, category, date){
+		if(this.expenses[0].name === 'Example'){
+			this.expenses = [this.createExpense(name, amount, category, date)];
+		}else{
+			this.expenses.push(this.createExpense(name, amount, category, date));
+		}
+	}
+
+	Accordion(){
+		const useStyles = makeStyles(theme => ({
+			root: {
+				width: '100%',
+			},
+			heading: {
+				fontSize: theme.typography.pxToRem(15),
+				fontWeight: theme.typography.fontWeightRegular,
+			},
+		}));
+
+		const classes = useStyles();
+
+		return (
+			<div className={classes.root}>
+				<ExpansionPanel>
+					<ExpansionPanelSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<Typography className={classes.heading}>
+							This Month's Expenses
+						</Typography>
+					</ExpansionPanelSummary>
+					<ExpansionPanelDetails>
+						<Typography>
+							<this.ExpenseTable />
+						</Typography>
+					</ExpansionPanelDetails>
+				</ExpansionPanel>
+			</div>
+		);
+	}
+	ExpenseTable(){
+		const useStyles = makeStyles({
+			root: {
+				width: '100%',
+				overflowX: 'auto',
+			},
+			table: {
+				minWidth: 650,
+			},
+		});
+		
+		this.expenses = [this.createExpense('Whole Foods', 45, 'Groceries', '11/6/2019')];
+		this.expenses.push(this.createExpense('Movie', 30, 'Entertainment', '10/31/2019'));
+		this.expenses.push(this.createExpense('Disneyland', 300, 'Leisure', '11/10/2019'));
+		// addExpense('Whole Foods', 45, 'Groceries', '11/6/2019');
+		// addExpense('Movie', 30, 'Entertainment', '10/31/2019');
+		// addExpense('Disneyland', 300, 'Leisure', '11/10/2019');
+		const classes = useStyles();
+		return (
+			<Paper className={classes.root}>
+				<Table className={classes.table} aria-label="Expenses">
+					<TableHead>
+						<TableRow>
+							<TableCell>Expense</TableCell>
+							<TableCell align="right">Amount</TableCell>
+							<TableCell align="right">Category</TableCell>
+							<TableCell align="right">Date</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{this.expenses.map(expenses => (
+							<TableRow key = {expenses.index}>
+								<TableCell component="th" scope="row">
+									{expenses.name}
+								</TableCell>
+								<TableCell align="right">{expenses.amount}</TableCell>
+								<TableCell align="right">{expenses.category}</TableCell>
+								<TableCell align="right">{expenses.date}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</Paper>
+		);
+	}
+
 	render(){
 		// Console.log({this.state.DATA});
 		return (
 			<div>
-				<Accordion />
-				<ExpenseFormDialog />
+				<this.Accordion />
+				<ExpenseForm />
 			</div>
 		);
 	}
