760f3eaedfde779284e92d83a078f2b230057849
Daniel Kwong <dkwon014@ucr.edu>
2019-12-03 03:15:28 -0800

    Test travis

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root") || document.createElement("div")
+  document.getElementById('root') || document.createElement('div')
 );
 
 // If you want your app to work offline and load faster, you can change
